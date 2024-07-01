7765068be061132e9654aa48ad134b809e88b05e
YourdenKerstens <125453773+YourdenKerstens@users.noreply.github.com>
2024-07-01 10:15:29 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar";
+import Content from "./Components/Content";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,30 @@
+
+const Footer = () => {
+  return (
+<div class="container my-5">
+
+  <section class="">
+  <footer class="text-center text-black">
+    <div class="container p-4 pb-0">
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-2">Register for free</span>
+          <button type="button" class="btn btn-outline-dark btn-rounded text-black ">
+            Sign up!
+          </button>
+        </p>
+      </section>
+    </div>
+
+    <div class="text-center p-1" >
+      © 2020 Copyright:
+      <a class="text-black" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+  </footer>
+</section>
+  
+</div>
+  )
+}
+
+export default Footer
